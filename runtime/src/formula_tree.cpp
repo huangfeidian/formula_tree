df67fc0817eb30f52f0015141380b1760e94c9a8
huang huiliang <spiritsaway@outlook.com>
2025-02-08 01:55:06 +0800

    增加安装脚本

--- runtime/src/formula_tree.cpp
+++ runtime/src/formula_tree.cpp
@@ -4,7 +4,7 @@
 #include <any_container/decode.h>
 #include <filesystem>
 #include <fstream>
-#include <magic_enum/magic_enum.hpp>
+#include <magic_enum.hpp>
 #include <sstream>
 using namespace spiritsaway::formula_tree::runtime;
 
